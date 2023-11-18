2ea9be6aaa562c18bdba692b4d86059562c16d4b
Guilherme Penteado <guilhermep2003@usp.br>
2023-11-18 15:26:16 -0300

    Verificação 7

--- blogfamiliar/settings.py
+++ blogfamiliar/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,7 +88,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600,
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-# adicione linhas abaixo
 python manage.py migrate
 cat <<EOF | python manage.py shell
 import os
